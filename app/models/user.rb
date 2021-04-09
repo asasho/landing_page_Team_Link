9e1c6e8ddbd409e27a7ed5a8b037e2cf658e534d
Ubuntu <ubuntu@ip-172-31-31-219.us-east-2.compute.internal>
2021-04-09 13:04:10 +0000

    Finish ch09

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,35 @@
 class User < ApplicationRecord
-    before_save { email.downcase! }
-    before_save { self.email = email.downcase }
-    validates :name,  presence: true, length: { maximum: 50 }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
-    has_secure_password
-    validates :password, presence: true, length: { minimum: 6 }
+  attr_accessor :remember_token
+  before_save { self.email = email.downcase }
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
     
-    def User.digest(string)
-      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                    BCrypt::Engine.cost
-      BCrypt::Password.create(string, cost: cost)
-    end
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+    
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+    
+  def remember
+    self.remember_token = User.new_token
+    self.update_attribute(:remember_digest, User.digest(remember_token))
+  end
+    
+#渡されたトークンがダイジェストと一致したらtrueを返す    
+  def authenticated?(remember_digest)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+    
+#ユーザのログイン情報を破棄する    
+  def forget
+    self.update_attribute(:remember_digest, nil)
+  end
 end
