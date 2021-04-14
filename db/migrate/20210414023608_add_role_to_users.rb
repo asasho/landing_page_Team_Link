b38fb1a0207edb81b0d53c55e44869a0701eb545
Ubuntu <ubuntu@ip-172-31-31-219.us-east-2.compute.internal>
2021-04-14 08:37:24 +0000

    Admin

--- db/migrate/20210414023608_add_role_to_users.rb
+++ db/migrate/20210414023608_add_role_to_users.rb
@@ -1,6 +1,5 @@
 class AddRoleToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :role, :integer, default: 0, null: false #デフォルトは0（一般ユーザー）にする。
-    add_column :users, :admin, :boolean, default: false
   end
 end
